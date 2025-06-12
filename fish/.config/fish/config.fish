bc916873e05506cc416f52d65e8996bb445a98e5
LieOnLion <lieonlion4@gmail.com>
2025-06-12 13:10:12 +0100

    moved_mkdot

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,8 +4,3 @@ export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 starship init fish | source
 
 alias stow-hm='stow -t ~'
-
-function mkdot
-    mkdir -p ./$argv[1]/$argv[2]
-    mv ~/$argv[2]/$argv[1] ./$argv[1]/$argv[2]/$argv[1]
-end
---
+++ fish/.config/fish/functions/mkdot.fish
@@ -0,0 +1,4 @@
+function mkdot
+    mkdir -p ./$argv[1]/$argv[2]
+    mv ~/$argv[2]/$argv[1] ./$argv[1]/$argv[2]/$argv[1]
+end
