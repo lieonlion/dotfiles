548a1edec7179076022babb0c6ab66bf48019ec1
LieOnLion <lieonlion4@gmail.com>
2025-06-12 15:37:02 +0100

    git_aliases_and_some_breakups

---
+++ fish/.config/fish/conf.d/aliases.fish
@@ -0,0 +1,22 @@
+# git
+
+alias gits='git status'
+alias gitd='git diff'
+
+alias gita='git add'
+alias gitcm='git commit'
+
+alias gitp='git push'
+alias gitf='git fetch'
+alias gitu='git pull'
+
+alias gitl='git log'
+alias gitb='git branch'
+alias gitch='git checkout'
+
+alias giti='git init'
+alias gitcl='git clone'
+
+# stow
+
+alias stow-hm='stow -t ~'
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,4 @@
 set fish_greeting
 fastfetch
-export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
-starship init fish | source
 
-alias stow-hm='stow -t ~'
+starship init fish | source
